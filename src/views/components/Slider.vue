758c03d7b160f5e36f3b87a927ccb1a1569c1c1a
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2022-09-13 01:46:23 -0300

    Update slider autoplay

--- src/views/components/Slider.vue
+++ src/views/components/Slider.vue
@@ -3,8 +3,11 @@
         <Swiper
             :modules="modules"
             :slides-per-view="1"
-            :space-between="50"
+            :space-between="0"
             loop
+            :autoplay="true"
+            :speed="700"
+            :
             navigation
             
         >
@@ -26,11 +29,12 @@
     import imgUrl2 from '../../assets/img/slide-2.jpg'
     import imgUrl3 from '../../assets/img/slide-3.jpg'
     import { Swiper, SwiperSlide } from 'swiper/vue';
-    import { Navigation } from 'swiper';
+    import { Autoplay, Navigation } from 'swiper';
     import 'swiper/css';
     import 'swiper/css/navigation';
+    import 'swiper/css/autoplay';
 
-    let modules = [Navigation];
+    let modules = [Navigation, Autoplay];
 
     let slides = [
         {
